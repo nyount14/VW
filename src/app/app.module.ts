cd31d5e7386bf5b3a92a2d7df66b07b2e9e3be8a
nyount14 <90732548+nyount14@users.noreply.github.com>
2022-07-01 20:29:36 -0500

    add'l components and routing
    added home component
    added notfound component
    adjusted routing order in app routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesModule } from './categories/categories.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     CategoriesModule,
-    TransactionsModule
+    TransactionsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
